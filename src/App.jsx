e46aeffacde4f5e3e6181308102136c1687809f8
deepika <deepikajagtap01@gmail.com>
2026-03-17 18:51:14 +0530

    deepika  commit

--- src/App.jsx
+++ src/App.jsx
@@ -74,6 +74,12 @@ function App() {
 
       <Footer />
       <EnquiryModal />
+       <button
+        onClick={() => window.scrollTo({ top: 10, behavior: "smooth" })}
+        className="fixed bottom-5 right-5 bg-blue-500 text-white px-4 py-2 rounded-full shadow-lg transition-opacity duration-300"
+      >
+        ↑
+      </button>
     </>
   )
 }
