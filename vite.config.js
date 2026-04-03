365d6cb2052fab58ec1f1debba84f24db95a89eb
harshparadkar25 <harshparadkar25@gmail.com>
2026-04-03 19:38:48 +0530

    vite

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+
+
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+const isSSR = !!process.env.SSR
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    ...(isSSR && { ssr: "src/entry-server.jsx" }),
+    ssrManifest: !isSSR,
+    outDir: isSSR ? "dist/server" : "dist/client",
+    rollupOptions: {
+      ...(isSSR && {
+        external: ["react", "react-dom"],
+      }),
+      output: {
+        ...(isSSR ? {} : {
+          manualChunks: {
+            "vendor-react": ["react", "react-dom", "react-router-dom"],
+            "vendor-motion": ["framer-motion"],
+            "vendor-icons": ["@fortawesome/react-fontawesome", "@fortawesome/free-solid-svg-icons", "@fortawesome/fontawesome-svg-core"],
+            "vendor-ui": ["lucide-react", "class-variance-authority", "clsx", "tailwind-merge"],
+          },
+        }),
+      },
+    },
+  },
+})
