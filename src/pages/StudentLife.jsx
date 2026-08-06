256d676885873ce429e6832453b5d8c7fe3f3f88
mayurchadokar <mayurchadokar14@gmail.com>
2026-08-06 11:27:44 +0530

    this chanegs

--- src/pages/StudentLife.jsx
+++ src/pages/StudentLife.jsx
@@ -85,13 +85,6 @@ const StudentLife = () => {
         ctaText={hero.cta_text}
         ctaLink={hero.cta_link}
       />
-      {sections?.testimonials && (
-        <StudentTestimonials
-          title={sections.testimonials.title}
-          testimonials={sections.testimonials.items}
-        />
-      )}
-      <ScratchSections sections={sections} exclude={['hero', 'a_calendar_full_of', 'a_calender_full_of', 'testimonials']} />
       <ActivitiesSlider
         title={calendarSection.title}
         content={calendarSection.content}
@@ -99,6 +92,13 @@ const StudentLife = () => {
         collegeSlug={collegeSlug}
         gallery={gallerySection.images || []}
       />
+      {sections?.testimonials && (
+        <StudentTestimonials
+          title={sections.testimonials.title}
+          testimonials={sections.testimonials.items}
+        />
+      )}
+      <ScratchSections sections={sections} exclude={['hero', 'a_calendar_full_of', 'a_calender_full_of', 'testimonials']} />
     </div>
   );
 };
